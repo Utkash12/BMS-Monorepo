13febb2c0f9fdc3dc26ae1e6415995188dc1b30e
Utkash12 <164newshubhamcity@gmail.com>
2025-04-03 15:38:26 +0530

    feat: implement interface of author, book, category for apigateway

---
+++ packages/apigateway/src/interfaces/book-interface.ts
@@ -0,0 +1,8 @@
+export interface BookInterface {
+    bookId: string;
+    bookTitle: string;
+    bookIsbn: string;
+    bookPrice: number;
+    bookAuthorId: string;
+    bookCategoryId: string;
+  }
